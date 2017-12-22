8558fb3250db8c45e8b7ce91e4ea808aaef37ef6
曾令燊 <dvsseed@users.noreply.github.com>
2017-12-22 11:28:20 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,50 +1,58 @@
 /**
-    Button A: play melody
-    Button B: show led and turn on vibration motor
-    Button A+B: increment by 1
-    Shake: led scanner
-*/
+ * Taibit blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace taibit {
+    /** function */
+    function delay_LED(item: number) {
+        led.plot(item, 2)
+        basic.pause(200)
+        led.unplot(item, 2)
+    }
 
-let a = 0
-basic.forever(() => {
-    basic.pause(100)
-})
+    /**
+      * Show LED Matrix to back and forth for n times
+      * @param ns describe parameter here, eg: 2
+      */
+    //% block
+    export function delayleds(ns: number) {
+        while (ns > 0) {
+            for (let i = 0; i <= 4; i++) {
+                delay_LED(i)
+            }
+            for (let i = 4; i >= 0; i--) {
+                delay_LED(i)
+            }
+            ns--
+        }
+    }
 
-function showled() {
-    basic.clearScreen()
-    basic.showIcon(IconNames.Square)
-    basic.pause(100)
-    basic.showIcon(IconNames.SmallSquare)
-    basic.pause(100)
-    images.createImage(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `).showImage(0)
-    basic.pause(100)
-    led.unplot(2, 2)
-}
+    /** function - Vibration Motor */
+    function vibration_Motor(sw: number) {
+        if (sw > 1) {
+            return
+        }
+        pins.digitalWritePin(DigitalPin.P12, sw)
+    }
 
-input.onButtonPressed(Button.A, () => {
-    basic.clearScreen()
-    music.beginMelody(music.getMelody(a), MelodyOptions.Once)
-    basic.showNumber(a + 1)
-})
+    /**
+      * Control Vibration Motor (pin12) => on | off
+      * @param sw describe parameter here, eg: 0
+    */
+    //% block
+    export function vibrationMotor(sw: number) {
+        vibration_Motor(sw)
+    }
 
-input.onButtonPressed(Button.B, () => {
-    showled()
-    taibit.vibrationMotorPause(1000)
-})
+    /**
+      * Control Vibration Motor (pin12) => pause millisecond
+      * @param ms describe parameter here, eg: 100
+    */
+    //% blockId="vibration_motor_pause" block="vibration motor pause (ms)| %ms"
+    export function vibrationMotorPause(ms: number) {
+        vibration_Motor(1)
+        basic.pause(ms)
+        vibration_Motor(0)
+    }
 
-input.onButtonPressed(Button.AB, () => {
-    basic.clearScreen()
-    a = (a + 1) % 20
-    basic.showNumber(a + 1)
-})
-
-input.onGesture(Gesture.Shake, () => {
-    basic.clearScreen()
-    taibit.delayleds(2)
-})
+} 
